7c8b6c244c1abfd9bb62517f8d8ba3dc34981f36
Hassan Amir <hamir-sajjad@myseneca.ca>
2024-02-03 21:03:46 -0500

    initial commit

---
+++ modules/unCountries.js
@@ -0,0 +1,99 @@
+const countryData = require("../data/countryData");
+const regionData = require("../data/regionData");
+
+let countries = [];
+
+function initialize() {
+  
+  return new Promise((resolve) => {
+
+    countries = [];
+
+    countryData.forEach((country) => {
+
+      const regionId = country.regionId;
+
+      const region = regionData.find((region) => region.id === regionId);
+
+      const countryWithRegion = { ...country, region };
+
+      countries.push(countryWithRegion);
+
+    });
+  
+    resolve(countries);
+
+  });
+}
+
+function getAllCountries() {
+
+  return new Promise((resolve) => {
+
+    resolve(countries);
+
+  });
+
+}
+
+function getCountryByCode(countryCode) {
+
+  return new Promise((resolve, reject) => {
+
+    const country = countries.find(
+
+      (c) => c.a2code.toLowerCase() === countryCode.toLowerCase()
+
+    );
+
+    if (country) {
+
+      resolve(country);
+
+    } else {
+
+      reject('Error: Unable to find the specified country.');
+
+    }
+
+  });
+
+}
+
+function getCountriesByRegion(region) {
+
+  return new Promise((resolve, reject) => {
+
+    const matchingCountries = countries.filter((country) =>
+
+      country.region.name.toLowerCase().includes(region.toLowerCase())
+
+    );
+
+
+    if (matchingCountries.length > 0) {
+
+      resolve(matchingCountries);
+
+    } else {
+
+      reject('Error: Unable to find the requested countries in the specific region.');
+
+    }
+  });
+}
+
+module.exports = {
+
+  initialize,
+
+  getAllCountries,
+
+  getCountryByCode,
+
+  getCountriesByRegion,
+
+};
+
+
+
---
+++ server.js
@@ -0,0 +1,70 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: ____Hassan Amir_____ Student ID: __145354221__ Date: ___03.02.2024____
+*
+*  Online (Cyclic) Link: 
+*
+********************************************************************************/
+
+
+const unCountryData = require("./modules/unCountries");
+
+const express = require('express');
+
+const app = express();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+    app.get('/', (req, res) => {
+      res.send(`Assignment 2: Hassan Amir - 145354221 `); 
+    });
+
+    app.get('/un/countries', (req, res) => {
+
+      unCountryData.getAllCountries()
+
+        .then(countries => res.send(countries))
+
+        .catch(err => res.send(err));
+
+    });
+
+
+    app.get('/un/countries/code-demo', (req, res) => {
+
+      const countryCode = 'PK';   
+
+      unCountryData.getCountryByCode(countryCode)
+
+        .then(data => res.send(data))
+
+        .catch(err => res.send(err));
+
+    });
+
+    app.get('/un/countries/region-demo', (req, res) => {
+
+      const regionName = 'asia'; 
+
+      unCountryData.getCountriesByRegion(regionName)
+
+        .then(data => res.send(data))
+
+        .catch(err => res.send(err));
+
+    });
+
+
+    unCountryData.initialize()
+
+  .then(() => {
+
+    app.listen(HTTP_PORT, () => console.log(`Server listening on: ${HTTP_PORT}`));
+
+  })
+
+  .catch(err => console.log("Error ", err));
